6b9f14e87dfe02ef8a85e5de80cdd8a2878377ff
ngduc <ducjava@gmail.com>
2018-10-15 20:27:44 -0700

    chore: update example

--- src/ReactTabulatorExample.tsx
+++ src/ReactTabulatorExample.tsx
@@ -11,18 +11,27 @@ const columns = [
 ];
 const data = [
   { id: 1, name: 'Oli Bob', age: '12', col: 'red', dob: '', rating: 5, passed: true },
-  { id: 2, name: 'Mary May', age: '1', col: 'blue', dob: '14/05/1989', rating: 4, passed: true },
+  { id: 2, name: 'Mary May', age: '1', col: 'green', dob: '14/05/1989', rating: 4, passed: true },
   { id: 3, name: 'Christine Lobowski', age: '42', col: 'green', dob: '22/05/1985', rating: 4, passed: false },
-  { id: 4, name: 'Brendon Philips', age: '125', col: 'orange', dob: '01/08/1980', rating: 4.5, passed: true },
+  { id: 4, name: 'Brendon Philips', age: '125', col: 'red', dob: '01/08/1980', rating: 4.5, passed: true },
   { id: 5, name: 'Margret Marmajuke', age: '16', col: 'yellow', dob: '31/01/1999', rating: 4, passed: false },
   { id: 6, name: 'Van Ng', age: '37', col: 'green', dob: '06/15/1982', rating: 4, passed: true },
   { id: 7, name: 'Duc Ng', age: '37', col: 'yellow', dob: '10/15/1982', rating: 4, passed: true }
 ];
 
+// Editable Example:
+const colorOptions = { ['']: '', red: 'red', green: 'green', yellow: 'yellow' };
 const editableColumns = [
   { title: 'Name', field: 'name', width: 150, editor: 'input', headerFilter: 'input' },
   { title: 'Age', field: 'age', align: 'left', formatter: 'progress', editor: 'progress' },
-  { title: 'Favourite Color', field: 'col', editor: 'input' },
+  {
+    title: 'Favourite Color',
+    field: 'col',
+    editor: 'select',
+    editorParams: colorOptions,
+    headerFilter: true,
+    headerFilterParams: colorOptions
+  },
   { title: 'Date Of Birth', field: 'dob', align: 'center', editor: 'input' },
   { title: 'Rating', field: 'rating', align: 'center', formatter: 'star', editor: true },
   { title: 'Passed?', field: 'passed', align: 'center', formatter: 'tickCross', editor: true }
